6476fcc008895feac7863c2be17bae6c1672b257
sambapython@gmail.com <sambapython@gmail.com>
2019-02-02 13:10:16 +0530

    models and prooject description.

---
+++ leavemanagement/main/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class User(AbstractUser):
+	user_types=[('e',"Employee"),('m',"Manager")]
+	phone=models.CharField(max_length=10)
+	type=models.CharField(choices=user_types, max_length=1)
+
+class LeaveType(models.Model):
+	name=models.CharField(max_length=250)
+	count=models.IntegerField()
+
+class Leave(models.Model):
+	desc=models.TextField(blank=True, null=True)
+	type=models.ForeignKey(LeaveType, on_delete=models.PROTECT)
+	# models.CASCADE: if you delete leave type, then it will delete all the leaves belongs 
+	# to that type
+	#models.PROTECT: if you delete any leave type, it wont allow you to delete the type
+	# if it has any leaves with that type in the leave table
+	date=models.DateTimeField(default=datetime.now)
+	leavedate=models.DateField()
+	user=models.ForeignKey(User, on_delete=models.PROTECT)
